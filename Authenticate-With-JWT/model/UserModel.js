65e81a88f9c76df40d830e70c167f29a4d736d0a
AmanKumarWeb19 <akgupta0524@gmail.com>
2024-07-16 12:24:42 +0530

    add more item in schema

--- Authenticate-With-JWT/model/UserModel.js
+++ Authenticate-With-JWT/model/UserModel.js
@@ -3,7 +3,9 @@ const mongoose = require("mongoose");
 const userSchema = mongoose.Schema(
   {
     email: { type: String, required: true },
-    password: { type: Number, required: true },
+    password: { type: String, required: true },
+    location: { type: String, required: true },
+    age: { type: Number, required: true },
   },
   {
     versionKey: false,
