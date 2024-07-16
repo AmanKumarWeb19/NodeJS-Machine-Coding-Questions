8bd69b5583ef17741a04891c9a870fb2135a14a7
AmanKumarWeb19 <akgupta0524@gmail.com>
2024-07-16 12:56:31 +0530

    login and register functionality has been created till now

--- Authenticate-With-JWT/controller/userController.js
+++ Authenticate-With-JWT/controller/userController.js
@@ -1,9 +1,25 @@
+const UserModel = require("../model/UserModel");
+
 const postRegister = async (req, res) => {
-  res.status(200).send("Register has been Done!");
+  try {
+    const newUser = new UserModel(req.body);
+    await newUser.save();
+    res.status(200).send({ msg: "Register has been Done!" });
+  } catch (error) {
+    res.status(400).send({ msg: error.message });
+  }
 };
 
 const postLogin = async (req, res) => {
-  res.status(200).send("Login has been Done!");
+  const { email, password } = req.body;
+  try {
+    const user = await UserModel.find({ email, password });
+    user.length > 0
+      ? res.status(200).send({ msg: "login successfull!" })
+      : res.status(400).send({ msg: "login failed!" }); 
+  } catch (error) {
+    res.status(400).send({ msg: error.message });
+  }
 };
 
 module.exports = { postLogin, postRegister };
